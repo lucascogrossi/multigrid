995cd3ea1717b6ec032c14793247fb8d325bfd67
lucascogrossi <contact@lucascogrossi.com>
2026-03-07 16:39:25 -0300

    add 2D jacobi amortecido

--- 2D/smoothers_2d.h
+++ 2D/smoothers_2d.h
@@ -19,14 +19,21 @@ inline void jacobi(Grid2D& grid) {
     grid.u = u_new;
 }
 
-inline void jacobi_amortecido(Grid& grid) {
-    std::vector<double> u_new(grid.n + 1, 0.0);
+inline void jacobi_amortecido(Grid2D& grid) {
+    std::vector<double> u_new((grid.nx + 1) * (grid.ny+1), 0.0);
     double u_jacobi;
-    double omega = 2.0/3.0;
+    double omega = 4.0/5.0; // valor otimo para suavizacao
 
-    for (int i = 1; i < grid.n; i++) {
-        u_jacobi = (grid.u[i-1] + grid.u[i+1]) / 2.0 + (grid.h*grid.h / 2.0) * grid.f[i];
-        u_new[i] = grid.u[i] + omega * (u_jacobi - grid.u[i]);
+    for (int i = 1; i < grid.nx; i++) {
+        for (int j = 1; j < grid.ny; j++) {
+            u_jacobi = (grid.u[grid.idx(i-1, j)] +
+                        grid.u[grid.idx(i+1, j)] +
+                        grid.u[grid.idx(i, j-1)] +
+                        grid.u[grid.idx(i, j+1)]) / 4.0 +
+                        (grid.hx*grid.hx / 4.0) * grid.f[grid.idx(i,j)];
+
+            u_new[grid.idx(i, j)] = grid.u[grid.idx(i, j)] + omega * (u_jacobi - grid.u[grid.idx(i, j)]);
+        }
     }
     grid.u = u_new;
 }
