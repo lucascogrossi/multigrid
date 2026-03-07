bc6f9e44a140660bd17d85ecd270f8af32c4f205
lucascogrossi <contact@lucascogrossi.com>
2026-03-07 16:45:35 -0300

    add 2D SOR smoother

--- 2D/smoothers_2d.h
+++ 2D/smoothers_2d.h
@@ -51,10 +51,16 @@ inline void gauss_seidel(Grid2D& grid) {
 }
 
 // Gauss-Seidel Sobrerelaxado
-inline void sor(Grid& grid, double omega) {
-    for (int i = 1; i < grid.n; i++) {
-        double u_gs = (grid.u[i-1] + grid.u[i+1]) / 2.0 + (grid.h*grid.h / 2.0) * grid.f[i];
-        grid.u[i] = grid.u[i] + omega * (u_gs - grid.u[i]);
+inline void sor(Grid2D& grid, double omega) {
+    for (int i = 1; i < grid.nx; i++) {
+        for (int j = 1; j < grid.ny; j++) {
+            double u_gs = (grid.u[grid.idx(i-1, j)] +
+                           grid.u[grid.idx(i+1, j)] +
+                           grid.u[grid.idx(i, j-1)] +
+                           grid.u[grid.idx(i, j+1)]) / 4.0 +
+                          (grid.hx*grid.hx / 4.0) * grid.f[grid.idx(i,j)];
+            grid.u[grid.idx(i, j)] = grid.u[grid.idx(i, j)] + omega * (u_gs - grid.u[grid.idx(i, j)]);
+        }
     }
 }
 
