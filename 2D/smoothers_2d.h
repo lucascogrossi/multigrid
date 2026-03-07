c918bb94bb2c13b2b35e0d7070b81c041fe66a50
lucascogrossi <contact@lucascogrossi.com>
2026-03-07 16:42:08 -0300

    add 2D gauss-seidel smoother

--- 2D/smoothers_2d.h
+++ 2D/smoothers_2d.h
@@ -38,9 +38,15 @@ inline void jacobi_amortecido(Grid2D& grid) {
     grid.u = u_new;
 }
 
-inline void gauss_seidel(Grid& grid) {
-    for (int i = 1; i < grid.n; i++) {
-        grid.u[i] = (grid.u[i-1] + grid.u[i+1]) / 2.0 + (grid.h*grid.h / 2.0) * grid.f[i];
+inline void gauss_seidel(Grid2D& grid) {
+    for (int i = 1; i < grid.nx; i++) {
+        for (int j = 1; j < grid.ny; j++) {
+            grid.u[grid.idx(i, j)] = (grid.u[grid.idx(i-1, j)] +
+                                     grid.u[grid.idx(i+1, j)] +
+                                     grid.u[grid.idx(i, j-1)] +
+                                     grid.u[grid.idx(i, j+1)]) / 4.0 +
+                                     (grid.hx*grid.hx / 4.0) * grid.f[grid.idx(i,j)];
+        }
     }
 }
 
